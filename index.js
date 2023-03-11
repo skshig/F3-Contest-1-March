a0998cfac733ca06ac0385ada00f2604b6296c52
skshig <skshig@gmail.com>
2023-03-11 20:42:37 +0530

    F3 contest1 march

---
+++ index.js
@@ -0,0 +1,14 @@
+// document.getElementById('next').addEventListener('click', () => {
+//     console.log("Hi");
+// });
+
+function sportsDay(OpeningCeremony){
+    const score = {red:0,blue:0,green:0,yellow:0};
+    OpeningCeremony();
+}
+function OpeningCeremony(){
+    // const score = {red:0,blue0,green:0,yellow:0};
+    // console.log(score);
+
+}
+sportsDay(OpeningCeremony)
