eb7ca42f86db8bee0252b55ea698d5352af53364
skshig <skshig@gmail.com>
2023-03-11 23:13:54 +0530

    F3 contest1 march

--- index.js
+++ index.js
@@ -2,46 +2,79 @@
 //     console.log("Hi");
 // });
 
-// function sportsDay(OpeningCeremony){
-//     const score = {red:0,blue:0,green:0,yellow:0};
-//     OpeningCeremony();
-// }
-const randomInt = (Math.random()*(15 - 10 + 1) + 10);
+function sportsDay(){
+    // const score = {red:0,blue:0,green:0,yellow:0};
+    OpeningCeremony(Race100M);
+}
+// const timeObj = { };
+// const color_time_arr = []
+//
+document.addEventListener('click', sportsDay);
+// const randomInt = (Math.random()*(15 - 10 + 1) + 10);
 function OpeningCeremony(Race100M){
-    console.log("Let the game begin")
-    const score = {red:0,blue:0,green:0,yellow:0};
-    Race100M(score,LongJump);
+    setTimeout(function() {
+        console.log("Let the game begin")
+        let score = {red:0,blue:0,green:0,yellow:0};
+        Race100M(score, LongJump)
+    },1000)
+    
+    
 }
-function Race100M(score,LongJump){
-    const score = {red:randomInt,
-        blue:randomInt,
-        green:randomInt,
-        yellow:randomInt};
-    LongJump(score,HighJump)
+ function Race100M(score){
+    setTimeout(function(){
+        console.log("hi");
+        score = race100m_scores()
+        LongJump(score);},3000)
+        
 }
-function LongJump(score,HighJump){
+function race100m_scores() {
+    let result = {
+        red: 0,
+        blue: 0,
+        green: 0,
+        yellow: 0
+    }
+    red_card = { color: "red", time: Math.floor(Math.random() * 6) + 10 };
+    blue_card = { color: "blue", time: Math.floor(Math.random() * 6) + 10 };
+    green_card = { color: "green", time: Math.floor(Math.random() * 6) + 10 };
+    yellow_card = { color: "yellow", time: Math.floor(Math.random() * 6) + 10 };
+
+    players_array = [red_card, green_card, blue_card, yellow_card]
+    // console.log(players_array);
+    players_array.sort((a, b) => { return a.time - b.time; })
+    // console.log(players_array);
+    result[players_array[0].color] = 50;
+    result[players_array[1].color] = 25;
+    return result
+}
+function LongJump(score){
+    setTimeout(function(){
     const user = prompt("What colour secured the highest jump?")
-    if(user = "red"){
+    if(user === "red"){
         score.red = (score.red) +  100;
     }
-    if(user = "yellow"){
+    if(user === "yellow"){
         score.yellow = (score.yellow) +  100;
     }
-    if(user = "green"){
+    if(user === "green"){
         score.green = (score.green) +  100;
     }
-    if(user = "blue"){
+    if(user === "blue"){
         score.blue = (score.blue) +  100;
     }
     else {
-        (user == "" || user == null){
+        if(user == "" || user == null){
         console.log("Event was cancelled");
     }
-    const score = 
+    // const score =
     HighJump(score,AwardCeremony);
     }
+    },2000)
+}
+function HighJump(score,AwardCeremony){
+    
+    AwardCeremony(score);
 }
 function AwardCeremony(score){
 
 }
-OpeningCeremony(Race100M)
